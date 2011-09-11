ab59e574dc944c1f504cd99c8c5b16e58526a249
Linmiao Xu <linmiao.xu@gmail.com>
2011-09-10 22:06:30 -0700

    Sync pausing/unpausing of game with the song.

--- public/js/mylibs/game.js
+++ public/js/mylibs/game.js
@@ -1,7 +1,8 @@
 $(document).ready(function() {
 	Crafty.init(60, 500,500);
 	Crafty.canvas();
-	
+	Crafty.pause();    // Game is paused at first.
+
 	// preload the needed assets
 	Crafty.load(["img/asprite.png", "img/abg.png"], function() {
 		// splice the spritemap
