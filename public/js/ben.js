b0fed724add113d44b5484b609c6cc48081fbfa7
iiilx <iiilx@ubuntu.(none)>
2011-09-10 22:06:31 -0700

    added song analysis  link retrieval function, data.js

--- public/js/ben.js
+++ public/js/ben.js
@@ -6,14 +6,15 @@ API_KEY = 'PAHHVTZEZEZLX9WQB';
 
 function get_song_id(artist, title) {
     $.ajax({
-        url: 'http://developer.echonest.com/api/v4/song/search?api_key=' + API_KEY + '&format=json&results=5&artist='+encodeURIComponent(artist)+'&title='+encodeURIComponent(title),
+        url: '/api/v4/song/search?api_key=' + API_KEY + '&format=json&results=5&artist='+encodeURIComponent(artist)+'&title='+encodeURIComponent(title),
         method: 'GET',
         success: function(data) {
                 //for each song in results, show artist and title and a link to click and get the audio
             $("div#player ul").html('');
             console.dir(data.response);
             for(var i=0; i < data.response.songs.length; i++) {
-                var url=get_song_url(data.response.songs[i].id);
+                var url='http://akjigga.com/wp-content/uploads/2011/07/Digital-Love-ALGERONICS-remix.mp3';
+                //var url=get_song_url(data.response.songs[i].id);
                 var choice = $('<li><a href="'+ url +'" id="'+data.response.songs[i].id+'">'+data.response.songs[i].artist_name +': '+ data.response.songs[i].title +'</a></li>');
                 $('div#player ul').prepend(choice);
             };
@@ -29,8 +30,7 @@ function get_song_url(song_id) {
     );*/
 
     $.ajax({
-        url:'http://developer.echonest.com/api/v4/artist/audio?api_key=' + API_KEY + '&id=' + song_id + '&format=json&results=1&start=0',
-        header:{'Access-Control-Allow-Origin':'*'},
+        url:'/api/v4/artist/audio?api_key=' + API_KEY + '&id=' + song_id + '&format=json&results=1&start=0',
         success: function(data) {
             console.log(data.response.audio.url);
             //$('#url').html('<a href="'+ data.response.audio.url +'">' + artist + ': '+ song +'</a>');
@@ -39,11 +39,39 @@ function get_song_url(song_id) {
    
 };
 
+function get_song_analysis_link() {
+    var id='TRXXHTJ1294CD8F3B3';
+   $.ajax({
+            type:'POST',
+            //contentType:'multipart/form-data',
+            data: {
+                'api_key': API_KEY,
+                'format':'json',
+                'id':id,
+                'bucket':'audio_summary'
+            },
+            url:'/api/v4/track/analyze',
+           success: function(data) {
+                console.log(data.response.track.audio_summary.analysis_url);
+            //    song_analysis(data.response.track.audio_summary.analysis_url);
+            }
+    }); 
+};
+
+function song_analysis(analysis_url) {
+  $.ajax({
+        url:analysis_url,
+        success: function(data){
+            console.log(data)
+        }
+    });  
+};
+
 $(document).ready(function() {
     $('#search_form').submit(function(e) {
         e.preventDefault();
         var artist = $('#artist').val();
         var title = $('#title').val();
-        get_song_id(artist, title);
+        get_song_id('muse','starlight'); //artist, title);
     });
 });
