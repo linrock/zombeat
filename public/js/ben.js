3f531fb1ddd4a7eb210f3dbe564563d9612829db
iiilx <iiilx@ubuntu.(none)>
2011-09-10 19:26:47 -0700

    search form

---
+++ public/js/ben.js
@@ -0,0 +1,51 @@
+// need a function where input artist/song is queried to echo and eventually an mp3 url is given as a response.
+// if there are multiple matches, user can pick one.
+//
+//
+API_KEY = 'PAHHVTZEZEZLX9WQB';
+
+function get_song_id(artist, title) {
+    $.ajax({
+        url: 'http://developer.echonest.com/api/v4/song/search?api_key=' + API_KEY + '&format=json&results=5&artist='+encodeURIComponent(artist)+'&title='+encodeURIComponent(title),
+        method: 'GET',
+        success: function(data) {
+                //for each song in results, show artist and title and a link to click and get the audio
+            $("#choices").html('');
+            for(var i=0; i < 5; i++) {
+                console.log(data.response.songs[i].id);
+                var choice = $('<p id="'+data.response.songs[i].id+'">'+ data.response.songs[i].artist_name +': '+ data.response.songs[i].title +'</p>');
+                $(choice).click(function() {
+                    get_song_url($(this).attr('id'));
+                    });
+                $('#choices').prepend(choice);
+            };
+        }
+    });
+};
+
+function get_song_url(song_id) {
+    /*$.getJSON('http://developer.echonest.com/api/v4/artist/audio?api_key=' + API_KEY + '&id=' + song_id + '&format=json&results=1&start=0',
+            function(data) {
+                console.log(data.response.audio.url);
+            }
+    );*/
+
+    $.ajax({
+        url:'http://developer.echonest.com/api/v4/artist/audio?api_key=' + API_KEY + '&id=' + song_id + '&format=json&results=1&start=0',
+        header:{'Access-Control-Allow-Origin':'*'},
+        success: function(data) {
+            console.log(data.response.audio.url);
+            //$('#url').html('<a href="'+ data.response.audio.url +'">' + artist + ': '+ song +'</a>');
+        }
+    });
+   
+};
+
+$(document).ready(function() {
+    $('#search_form').submit(function(e) {
+        e.preventDefault();
+        var artist = $('#artist').val();
+        var title = $('#title').val();
+        get_song_id(artist, title);
+    });
+});
